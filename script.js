f49cc2f3d66b7f9d38292ad5ea2160c92df240d4
wendel <wendelborges62@gmail.com>
2023-04-26 15:44:01 -0300

    requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,9 @@
+const email = document.querySelector(".email")
+const senha = document.querySelector(".senha")
+const btn = () => { 
+    if(email.value != "tryber@teste.com" || senha.value != '123456') {
+    alert('Email ou senha inválidos.');
+} else {
+    alert('Olá, Tryber!')
+}
+}
